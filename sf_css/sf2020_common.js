f5dc62ae7ff7408cfba509b124d933c9bab663fe
mkshin98_intellij <mkshin98@gamil.com>
2024-08-23 00:22:33 +0900

    2024-08 몽골 밤하늘 탐사

--- sf_css/sf2020_common.js
+++ sf_css/sf2020_common.js
@@ -2,12 +2,14 @@
  * 
  */
 
-function simpleLightbox(imageUrl, bgColor, maxWidth){
+function simpleLightbox(imageUrl, maxWidth, bgColor){
     if(typeof bgColor === 'undefined'){
         bgColor = '#000';
     }
     if(typeof maxWidth === 'undefined'){
         maxWidth = '1100px';
     }
-    window.open('', 'simpleLightbox').document.write('<html><head><meta name="viewport" content="user-scalable=yes, initial-scale=1.0, maximum-scale=5.0, minimum-scale=1.0, width=device-width" /></head><body style="margin:0;'+bgColor+';height:100%;" onclick="javascript:window.close(\'simpleLightbox\');"><table border="0" width="100%" height="100%"><tr><td valign="middle" align="center"><img style="position:relative;z-index:2;width:100%;max-width:'+maxWidth+';" src="'+imageUrl+'"/></td></tr></table></body></html>');
+    window.open('', 'simpleLightbox').document.write('<html><head><meta name="viewport" content="user-scalable=yes, initial-scale=1.0, maximum-scale=5.0, minimum-scale=1.0, width=device-width" /></head><body style="margin:0;'+bgColor+';height:100%;" ><table border="0" width="100%" height="100%"><tr><td valign="middle" align="center"><img style="position:relative;z-index:2;width:100%;max-width:'+maxWidth+';" src="'+imageUrl+'"/></td></tr></table></body></html>');
 }
+
+/* onclick="javascript:window.close('simpleLightbox');" */
